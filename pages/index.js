790585e971bf9b549db87cf808a451f71d5f7b48
Andrey Gomes <andreygdsn@gmail.com>
2024-11-23 16:25:36 -0300

    Em Construção Html Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Estamos Em Construção...</h1>;
+  return (
+    <div>
+      <h1>🔥JohnNews</h1>
+      <hr />
+      <h2>Estamos Em Construção...</h2>
+    </div>
+  );
 }
 
 export default Home;
