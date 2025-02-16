c7bc44260c5d307310382310e8a3d664a1a4b6da
Andrey Gomes <andreygdsn@gmail.com>
2025-02-16 17:21:52 -0300

    feat: add `MethodNoAllowedError`

--- infra/errors.js
+++ infra/errors.js
@@ -5,13 +5,32 @@ export class InternalServerError extends Error {
     });
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
+    this.status_code = 500;
   }
   toJSON() {
     return {
       name: this.name,
       message: this.message,
       action: this.action,
-      status_code: 500,
+      status_code: this.status_code,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para esse endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.message = "Método não permitido para esse endpoint.";
+    this.action = "Verifique se o método HTTP enviado é válido para esse endpoint.";
+    this.status_code = 405;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: 405,
     };
   }
 }
