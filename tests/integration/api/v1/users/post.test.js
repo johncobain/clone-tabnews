b2fcd2a3f3bcac7862dec5de944a32336a91441a
Andrey Gomes <andreygdsn@gmail.com>
2025-02-27 15:50:51 -0300

    chore: temp users test api before formating computer

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,26 @@
+import orquestrator from "tests/orquestrator.js";
+import database from "infra/database.js";
+
+beforeAll(async () => {
+  await orquestrator.waitForAllServices();
+  await orquestrator.clearDatabase();
+  await orquestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
+        values: ["johnCobain", "john@cobain.com", "senha123"],
+      });
+      const users = await database.query("SELECT * FROM users;");
+      console.log(users.rows);
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
