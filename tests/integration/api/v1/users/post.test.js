ffd7c93b8335ec8f338b2b236a3f90c4fe512736
Andrey <andreygdsn@gmail.com>
2025-02-28 17:26:52 -0300

    feat: create `user` model base

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
+import { version as uuidVersion } from "uuid";
 import orquestrator from "tests/orquestrator.js";
-import database from "infra/database.js";
 
 beforeAll(async () => {
   await orquestrator.waitForAllServices();
@@ -10,17 +10,33 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["johnCobain", "john@cobain.com", "senha123"],
-      });
-      const users = await database.query("SELECT * FROM users;");
-      console.log(users.rows);
-
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "johnCobain",
+          email: "john@cobain.com",
+          password: "senha123",
+        }),
       });
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "johnCobain",
+        email: "john@cobain.com",
+        password: "senha123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
   });
 });
